8dc94805b9437e240946dfcac37785c40c2f4bdb
Makers Laptop 22 <makerslaptop22@Makerss-MacBook-Pro-3.local>
2015-09-08 11:47:36 +0100

    first test is passing

---
+++ test/gitUserSearchController.spec.js
@@ -0,0 +1,13 @@
+describe('GitUserSearchController', function() {
+  beforeEach(module('GitUserSearch'));
+  var ctrl;
+
+  beforeEach(inject(function($controller) {
+    ctrl = $controller('GitUserSearchController');
+  }));
+
+  it('initialises with an empty seach result and term', function() {
+    expect(ctrl.searchResult).toBeUndefined();
+    expect(ctrl.searchTerm).toBeUndefined();
+  });
+});
