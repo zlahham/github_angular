2de213d459ce90cbeecae29e7adc53de09ed9418
Makers Laptop 22 <makerslaptop22@Makerss-MacBook-Pro-3.local>
2015-09-08 13:54:08 +0100

    doSearch method working to bring up dummy results

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -7,27 +7,29 @@ describe('GitUserSearchController', function() {
   }));
 
   it('initialises with an empty seach result and term', function() {
-    expect(ctrl.searchResult).toBeDefined();
+    expect(ctrl.searchResult).toBeUndefined();
     expect(ctrl.searchTerm).toBeUndefined();
   });
-});
 
-describe('when searching for a user', function() {
+  describe('when searching for a user', function() {
 
-  var items = [
-    {
-      "login": "tansaku",
-      "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-      "html_url": "https://github.com/tansaku"
-    },
-    {
-      "login": "stephenlloyd",
-      "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-      "html_url": "https://github.com/stephenlloyd"
-    }
-  ];
+    var items = [
+      {
+        "login": "tansaku",
+        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
+        "html_url": "https://github.com/tansaku"
+      },
+      {
+        "login": "stephenlloyd",
+        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
+        "html_url": "https://github.com/stephenlloyd"
+      }
+    ];
 
-  it('displays search results', function() {
-    expect(ctrl.searchResult.items).toEqual(items);
+    it('displays search results', function() {
+      ctrl.searchTerm = 'hello';
+      ctrl.doSearch();
+      expect(ctrl.searchResult.items).toEqual(items);
+    });
   });
 });
